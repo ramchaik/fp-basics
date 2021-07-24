7614fe6bfedd701cac5999a2eae0353f4ed17495
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-07-25 01:58:27 +0530

    feat: add case of independent tasks

--- src/asyncCompose.js
+++ src/asyncCompose.js
@@ -5,14 +5,15 @@ const composeAsync =
   (x) =>
     funcs.reduce(applyAsync, Promise.resolve(x));
 
+// ! Dependent tasks
 const allTaskPromise = (allTasks) =>
   allTasks.map(
     (task) => () =>
       task()
         .then(console.log)
         .catch((e) => {
-          console.log('Error: ', e); // just log, all settled like 
-          // throw e; // like Promise.all 
+          console.log('Error: ', e);
+          throw e;
         })
   );
 
@@ -22,7 +23,7 @@ const runAll = (...fns) => composeAsync(...allTaskPromise(fns))();
 
 const promise1 = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('promise #1 in 1 sec'), 1000);
+    setTimeout(() => resolve('promise #1 in 2 sec'), 2000);
   });
 };
 
@@ -34,15 +35,36 @@ const promise2 = () => {
 
 const promise3 = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('promise #3 in 1 sec'), 1000);
+    setTimeout(() => reject('promise #3 in 0.5 sec'), 500);
   });
 };
 
+// runAll(promise1, promise2, promise3)
+//   .then(() => {
+//     console.log('Final: you are done!');
+//   })
+//   .catch((e) => {
+//     console.log('Final: ', { e });
+//   });
 
-runAll(promise1, promise2, promise3)
+// ! Independent tasks
+// * all tasks are invoked synchronously, but handled asynchronously
+// * so every task will run irrespective of any one failing
+Promise.all(
+  [promise1, promise2, promise3].map((p) =>
+    p()
+      .then(console.log)
+      .catch((e) => {
+        console.log('from error: ', e);
+        throw e;
+      })
+  )
+)
   .then(() => {
+    // On successful completion of all tasks
     console.log('Final: you are done!');
   })
   .catch((e) => {
-    console.log('Final: ', { e });
+    // On failure of any task, error is propagated to this final state
+    console.log('Any one task failed: ', { e });
   });
